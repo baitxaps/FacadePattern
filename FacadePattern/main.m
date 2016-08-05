5b08885dba054607b21d8d80066e2126811a1fd5
rhc <bawfnhaps@163.com>
2016-08-05 22:46:03 +0800

    FacadePattern

--- FacadePattern/main.m
+++ FacadePattern/main.m
@@ -7,11 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ModenPostOffice.h"
+#import "Facade.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        ModenPostOffice *hellRoadPostOffice = [ModenPostOffice new];
+        
+        NSString *address = @"Happy Road No. 666,God Province,Heaven";
+        
+        NSString *context = @"Hello,It's me ,do u know who I am? I'm your ...";
+        
+        [hellRoadPostOffice sendLetter:context address:address];
+        
+        // 2
+        Facade *facade = [Facade new];
+        [facade mathodA];
+        [facade mathodB];
+        [facade mathodC];
+        
     }
     return 0;
 }
